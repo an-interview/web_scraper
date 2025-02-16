077ce0a771eac71fd345456651c01f7ba2be4263
Abhijit Navale <abhijitnwork@gmail.com>
2025-02-16 15:11:36 +0530

    Optimize Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,32 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.page(params[:page]).per(10)
-    respond_to do |format|
-      format.json { render json: @products }
-    end
+
+    render json: @products, each_serializer: ProductSerializer
   end
 
   def import
     url = params[:url]
 
-    if url.present?
+    if valid_url?(url)
       ImportProductJob.perform_later(url)
-      render json: {message: 'Product import has been initiated'}, status: :accepted
+      render json: { message: 'Product import has been initiated' }, status: :accepted
     else
-      Rails.logger.error "Missing URL"
-      render json: { error: "Missing URL" }, status: :unprocessable_entity
+      Rails.logger.error "Invalid Or Missing URL"
+      render json: { errors: "Invalid Or Missing URL" }, status: :bad_request
+    end
+  end
+
+  private
+
+  def valid_url?(url)
+    return false if url.blank?
+
+    begin
+      uri = URI.parse(url)
+      uri.kind_of?(URI::HTTP)
+    rescue URI::InvalidURIError
+      false
     end
   end
 end
