ec470a4e09ce88931bda9de5c533e15144e7faec
Abhijit Navale <abhijitnwork@gmail.com>
2025-02-16 15:11:17 +0530

    Optimize product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,8 @@ class Product < ApplicationRecord
   validates :description, length: { maximum: 1000 }, allow_blank: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
   validates :currency, presence: true, length: { is: 3 } # Example: "INR"
-  validates :image_url, format: { with: /\Ahttps?:\/\/.+\.(png|jpg|jpeg|webp|gif)\z/i, message: "must be a valid image URL" }, allow_blank: true
-  validates :source_url, format: { with: /\Ahttps?:\/\/.+\z/i, message: "must be a valid URL" }, presence: true
+  validates :image_url, format: { with: /\Ahttps?:\/\/.*\.(?:jpg|jpeg|png|gif|webp)(\?.*)?\z/i, message: "must be a valid image URL" }, allow_blank: true
+  validates :source_url, format: { with: /\Ahttps?:\/\/.+\z/i, message: "must be a valid URL" }, presence: true, uniqueness: true
   # validates :scraped_at, presence: true
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
   validates :review_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
