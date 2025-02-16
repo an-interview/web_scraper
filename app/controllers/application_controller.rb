7be6544217d513eb4f232c8b73d9e8f9eacd095a
Abhijit Navale <abhijitnwork@gmail.com>
2025-02-16 15:10:26 +0530

    Add very simple authentication for react frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_request
+
+  private
+
+  def authenticate_request
+    api_key = request.headers['Trip-Api-Key']
+
+    if api_key == 'ABCD' # Temporary. To be implemented with strong authentication in future.
+      return
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
