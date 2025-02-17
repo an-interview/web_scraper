e32580b9b92867eae887c3538ca5d7149d9ad65a
Abhijit Navale <abhijitnwork@gmail.com>
2025-02-17 12:16:14 +0530

    Clean Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::Base
 
   def authenticate_request
     api_key = request.headers['Trip-Api-Key']
-    ak = ENV["API_KEY"]
-    puts "Comparing API KEY with #{ak}"
 
     if api_key == ENV["API_KEY"] # Temporary. To be implemented with strong authentication in future.
       return
